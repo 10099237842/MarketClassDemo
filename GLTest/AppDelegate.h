7b99874ca27ad35e9e581a6fa56c06ed5cb1de9b
cendi <cendi@cendidemacbook-pro.local>
2018-11-06 14:56:34 +0800

    Initial Commit

---
+++ GLTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLTest
+//
+//  Created by cendi on 2018/11/6.
+//  Copyright © 2018 cendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GLTest
+//
+//  Created by cendi on 2018/11/6.
+//  Copyright © 2018 cendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
